b6aa07fff9df658431879b8acf0cad5f22adc788
Your Name <you@example.com>
2016-01-29 12:58:08 +0600

    Add GPIO

--- Switching.py
+++ Switching.py
@@ -7,21 +7,21 @@ class Switching(object):
     def __init__(self):
 
 
-        self.GPIO.setmode(GPIO.BOARD)
-        self.GPIO.setup(BOARD_PINS, GPIO.OUT)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(BOARD_PINS, GPIO.OUT)
         self.state =[0]*4
-        
+
 
     def turnOn(self,position):
 
         self.state[position] = 1
-        self.GPIO.output(position, GPIO.HIGH)
+        GPIO.output(position, GPIO.HIGH)
         print  'ON' , position
 
     def turnOff(self,position):
 
 
         self.state[position] = 0
-        self.GPIO.output(position, GPIO.LOW)
+        GPIO.output(position, GPIO.LOW)
         print  'Off' , position
 
