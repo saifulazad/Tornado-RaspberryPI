7ff3986f0785c65434acd28f4da9ff9f5128430e
Your Name <you@example.com>
2016-08-30 20:01:09 +0600

    FIX to Dict

--- Switching.py
+++ Switching.py
@@ -1,6 +1,5 @@
 __author__ = 'Azad'
 import RPi.GPIO as GPIO
-
 class Switching(object):
 
     def __init__(self, PINS):
@@ -8,20 +7,22 @@ class Switching(object):
         self.PINS = PINS
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.PINS, GPIO.OUT)
-        self.state =[0]*4
-        self.offset = 10
+        self.state =dict(zip(PINS,[0]*len(PINS)))
+
 
 
     def turnOn(self,position):
 
-        self.state[position - self.offset] = 1
+        self.state[position] = 1
         GPIO.output(position, GPIO.HIGH)
         print  'ON' , position
 
     def turnOff(self,position):
 
 
-        self.state[position - self.offset] = 0
+        self.state[position] = 0
         GPIO.output(position, GPIO.LOW)
         print  'Off' , position
 
+    def getState(self):
+        return self.state
